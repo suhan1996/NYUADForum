34db13f429ea08ca25e8a57d883d942f02c8a6fb
suhan1996 <hs2657@nyu.edu>
2017-09-27 21:08:14 +0400

    first

--- db.js
+++ db.js
@@ -25,27 +25,4 @@ mongoose.model('Comment', Comment);
 mongoose.model('Link', Link);
 mongoose.Promise = global.Promise;
 
-//mongoose.connect('mongodb://localhost/nyushit');
 mongoose.connect('mongodb://passwordis123456:123456@ds139124.mlab.com:39124/heroku_cftw89xp');
-//mongodb://<dbuser>:<dbpassword>@ds139124.mlab.com:39124/heroku_cftw89xp
-//
-//if (process.env.NODE_ENV === 'PRODUCTION') {
-//    // if we're in PRODUCTION mode, then read the configration from a file
-//    // use blocking file io to do this...
-//    var fs = require('fs');
-//    var path = require('path');
-//    var fn = path.join(__dirname, 'config.json');
-//    var data = fs.readFileSync(fn);
-//
-//    // our configuration file will be in json, so parse it and set the
-//    // conenction string appropriately!
-//    var conf = JSON.parse(data);
-//    var dbconf = conf.dbconf;
-//} else {
-//    // if we're not in PRODUCTION mode, then use
-//    dbconf = 'mongodb://localhost/final';
-//    console.log("else",dbconf)
-//}
-//console.log("final",dbconf)
-//
-//mongoose.connect(dbconf)
